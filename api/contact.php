c57b7294716f8c6be6bccd4c0a3bd534de16a36e
luchoaglp <lucho_aglp@hotmail.com>
2020-07-24 10:22:05 -0300

    Updated

---
+++ api/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+//ini_set('display_errors', 1);
+
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+echo json_encode("OK");
